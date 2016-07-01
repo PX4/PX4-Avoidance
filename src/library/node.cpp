760fd05da54cbc436273379aa0e4ab97b1b269a1
Vilhjalmur <vilhjalmur89@gmail.com>
2016-07-01 15:37:20 +0200

    Code clean-up

--- src/library/node.cpp
+++ src/library/node.cpp
@@ -2,4 +2,13 @@
 
 namespace avoidance {
 
+std::vector<Node> Node::getNeighbors() const {
+  std::vector<Node> neighbors;
+  neighbors.reserve(10);
+	for (Cell neighborCell : cell.getNeighbors()) {
+		neighbors.push_back(Node(neighborCell, cell));
+	}
+  return neighbors;
+}
+
 } // namespace avoidance
