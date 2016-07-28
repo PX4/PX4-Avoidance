0ee7ac7f65743f4b5798420a94f550ccdaf082d6
Vilhjalmur <vilhjalmur89@gmail.com>
2016-07-28 18:33:03 +0200

    Generalized risk function to work with SpeedNode

--- src/library/node.cpp
+++ src/library/node.cpp
@@ -18,13 +18,41 @@ NodePtr Node::nextNode(const Cell & nextCell) const {
   return NodePtr(new Node(nextCell, cell_));
 }
 
-std::vector<NodePtr > Node::getNeighbors() const {
+std::vector<NodePtr> Node::getNeighbors() const {
   std::vector<NodePtr > neighbors;
   neighbors.reserve(10);
 	for (Cell neighborCell : cell_.getNeighbors()) {
 		neighbors.push_back(nextNode(neighborCell));
 	}
   return neighbors;
+}
+
+std::unordered_set<Cell> Node::getCells() const {
+  std::unordered_set<Cell> cells;
+  int dx = cell_.x() - parent_.x();
+  int dy = cell_.y() - parent_.y();
+  int dz = cell_.z() - parent_.z();
+
+  int steps = 2 * std::max(std::abs(dx), std::max(std::abs(dy), std::abs(dz)));
+  
+  double x_step = (cell_.xPos() - parent_.xPos()) / steps;
+  double y_step = (cell_.yPos() - parent_.yPos()) / steps;
+  double z_step = (cell_.zPos() - parent_.zPos()) / steps;
+
+  for (int i = 1; i <= steps; ++i) {
+    double new_x = parent_.xPos() + x_step * i;
+    double new_y = parent_.yPos() + y_step * i;
+    double new_z = parent_.zPos() + z_step * i;
+    cells.insert(Cell(new_x + 0.1, new_y + 0.1, new_z));
+    cells.insert(Cell(new_x + 0.1, new_y - 0.1, new_z));
+    cells.insert(Cell(new_x - 0.1, new_y + 0.1, new_z));
+    cells.insert(Cell(new_x - 0.1, new_y - 0.1, new_z));
+  }
+  return cells;
+}
+
+double Node::getLength() const {
+  return parent_.distance3D(cell_);
 }
 
 // The number of 45 degree turns neede to go to other
