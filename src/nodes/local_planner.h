1105f08a09a3b8917ee776b15678caf05e45a38e
Martina <martina@rivizzigno.it>
2017-09-04 15:53:12 +0200

    add pre-calculated array with cost of all free directions

--- src/nodes/local_planner.h
+++ src/nodes/local_planner.h
@@ -131,6 +131,7 @@ public:
 	//std::vector<float> accumulated_height_prior{0.0, 0.000098, 0.000927, 0.0473, 0.011746, 0.020522, 0.03259, 0.07106, 0.13777, 0.2041, 0.2759, 0.3516};
     std::vector<float> accumulated_height_prior{1.0, 0.9999, 0.9990, 0.9952, 0.9882, 0.9794, 0.9674, 0.9289, 0.8622, 0.7958, 0.7240, 0.6483, 0.5752, 0.5132, 0.4535, 0.4020, 0.3525, 0.3090, 0.2670, 0.2300, 0.2066, 0.1831};
     std::vector<float> height_prior{0.000057, 0.00052, 0.00369, 0.01176, 0.0166, 0.01728, 0.04255, 0.11559, 0.1315, 0.1357, 0.1556, 0.1464};
+    std::vector<float> cost_path_candidates;
 
     std::vector<geometry_msgs::Point> extension_points;
     float coef1, coef2, coef3, coef4;
