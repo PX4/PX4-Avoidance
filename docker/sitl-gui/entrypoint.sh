593d7911c3416b4ba1d4129d50433e6e7d74f527
Jonas Vautherin <jonas.vautherin@gmail.com>
2017-09-20 16:19:09 +0200

    Add docker files allowing to build and run node

---
+++ docker/sitl-gui/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /opt/ros/kinetic/setup.bash
+source ${FIRMWARE_DIR}/Tools/setup_gazebo.bash ${FIRMWARE_DIR} ${FIRMWARE_DIR}/build_posix_sitl_default
+
+export ROS_MASTER_URI=http://mavros-avoidance:11311
+
+rviz -d /root/avoidance/resource/planning.rviz &
+GAZEBO_MASTER_URI=http://sitl-avoidance-server:11345 gzclient --verbose
