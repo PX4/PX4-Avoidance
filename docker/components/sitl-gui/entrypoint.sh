dd32973ddff1b0c4ea09ce3be4f948f8be5fb781
Jonas Vautherin <jonas.vautherin@gmail.com>
2017-10-12 10:07:06 +0200

    docker: add deploy variants for local planner

--- docker/components/sitl-gui/entrypoint.sh
+++ docker/components/sitl-gui/entrypoint.sh
@@ -5,5 +5,5 @@ source ${FIRMWARE_DIR}/Tools/setup_gazebo.bash ${FIRMWARE_DIR} ${FIRMWARE_DIR}/b
 
 export ROS_MASTER_URI=http://mavros-avoidance:11311
 
-rviz -d /root/avoidance/resource/planning.rviz &
+rviz -d $1 &
 GAZEBO_MASTER_URI=http://sitl-avoidance-server:11345 gzclient --verbose
