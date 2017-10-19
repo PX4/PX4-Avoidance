8d50ef97bff2efde3f5519942fb74a763fd7ac51
Jonas Vautherin <jonas.vautherin@gmail.com>
2017-10-19 18:16:19 +0200

    docker: simplify openvpn config further, now using a plaintext ptp connection

--- docker/global_planner/global-planner-prod/global-planner-prod-debug/run.sh
+++ docker/global_planner/global-planner-prod/global-planner-prod-debug/run.sh
@@ -9,14 +9,13 @@ route add -net 10.0.0.0/16 gw $VPN_SERVER_IP
 
 echo "The system has been setup in debug mode, with a vpn server allowing you to connect to the ROS master from a remote machine. To do so:
 
-1. From the remote machine, connect to the vpn, with "client.ovpn" the client config file. It is assuming a USB connection to the Aero, meaning the drone is available on 192.168.7.2. Edit the config file for a different setup.
-
-    $ sudo openvpn --config client.ovpn
-
-2. Set the MASTER_ROS_URI and ROS_IP on the remote machine:
+1. From the remote machine, run:
 
     $ export ROS_MASTER_URI=http://${ROS_MASTER_URI}:11311
-    $ export ROS_IP=<your vpn ip>
+    $ export ROS_IP=10.0.0.2
+    $ sudo openvpn --remote 192.168.7.2 --dev tun --ifconfig 10.0.0.2 10.0.0.1 --route 172.20.0.0 255.255.255.0
+
+Note that this assume the remote machine connects to the drone on 192.168.7.2 (which is the USB connection on the Intel Aero). This can be adapted to your needs.
 
 Press any key to continue..."
 
