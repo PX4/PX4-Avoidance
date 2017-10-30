eb51f126afcea454b8f7505e55627f3540a895c3
Jonas Vautherin <jonas.vautherin@gmail.com>
2017-10-30 10:52:01 +0100

    docker: improve prod-debug run script instructions

--- docker/local_planner/local-planner-prod/local-planner-prod-debug/run.sh
+++ docker/local_planner/local-planner-prod/local-planner-prod-debug/run.sh
@@ -9,13 +9,20 @@ ROS_MASTER_URI=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddr
 
 route add -net 10.0.0.0/16 gw $VPN_SERVER_IP
 
-echo "The system has been setup in debug mode, with a vpn server allowing you to connect to the ROS master from a remote machine. To do so:
+echo "The system has been setup in debug mode, with a vpn server allowing you to connect to the ROS master from a remote machine. To do so, follow these instructions from the remote machine:
 
-1. From the remote machine, run:
+1. Connect to the VPN from one terminal:
+
+    $ sudo openvpn --remote 192.168.7.2 --dev tun --ifconfig 10.0.0.2 10.0.0.1 --route 172.20.0.0 255.255.255.0
+
+2. In another terminal (say the \"ROS terminal\"), setup the ROS environment variables like so:
 
     $ export ROS_MASTER_URI=http://${ROS_MASTER_URI}:11311
     $ export ROS_IP=10.0.0.2
-    $ sudo openvpn --remote 192.168.7.2 --dev tun --ifconfig 10.0.0.2 10.0.0.1 --route 172.20.0.0 255.255.255.0
+
+3. From the ROS terminal, you can now run Rviz or check the topics with commands such as:
+
+    $ rostopic list
 
 Note that this assume the remote machine connects to the drone on 192.168.7.2 (which is the USB connection on the Intel Aero). This can be adapted to your needs.
 
