944ea4c839cb76a723eb92d1407d654358023432
Jonas Vautherin <jonas.vautherin@gmail.com>
2017-12-14 15:15:23 +0100

    docker: install mavros from official package instead of sources

--- docker/components/mavros-avoidance/entrypoint.sh
+++ docker/components/mavros-avoidance/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-source ~/catkin_ws/devel/setup.bash
-export ROS_IP=`hostname -I`
-export ROS_MASTER_URI=http://`hostname -I`:11311
+source /opt/ros/${ROS_DISTRO}/setup.bash
+export ROS_IP=${ROS_IP:-`hostname -I`}
+export ROS_MASTER_URI=${ROS_MASTER_URI:-http://`hostname -I`:11311}
 
 roscore -p 11311 &
 sleep 1
-rosparam set use_sim_time true
 
 roslaunch mavros -p 11311 px4.launch $1
--- docker/components/mavros/entrypoint.sh
+++ docker/components/mavros/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-source ~/catkin_ws/devel/setup.bash
-export ROS_IP=`hostname -I`
-export ROS_MASTER_URI=http://`hostname -I`:11311
-
-roscore -p 11311 &
-sleep 1
-rosparam set use_sim_time true
+source /opt/ros/${ROS_DISTRO}/setup.bash
+export ROS_IP=${ROS_IP:-`hostname -I`}
+export ROS_MASTER_URI=${ROS_MASTER_URI:-http://`hostname -I`:11311}
 
 roslaunch mavros -p 11311 px4.launch $1
