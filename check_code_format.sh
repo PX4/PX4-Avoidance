d17c5ca864b4c04737efe9e046bdf690cd1e6941
Martina <martina@rivizzigno.it>
2018-05-30 17:52:37 +0200

    check_code_format: use fix_style script to check for failure in style

--- check_code_format.sh
+++ check_code_format.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
 
-COLOR_END="\033[0m"
-COLOR_RED="\033[0;31m" 
-COLOR_GREEN="\033[0;32m" 
-STYLE='google'
+# Fix style recursively in all the repository
+sh fix_style.sh .
 
-FILES_TO_CHECK=$(git diff --name-only master | grep -E ".*\.(cpp|c|h|hpp)"$)
+# Print the diff with the remote branch (empty if no diff)
+git --no-pager diff -U0 --color
 
-if [ -z "${FILES_TO_CHECK}" ]; then
-  echo -e "No diff to master."
-  exit 0
-fi
-
-for f in $FILES_TO_CHECK; do
-  d=$(diff -u "$f" <(clang-format "$f" -style=${STYLE}))
-  if [ -z "$d" ]; then
-    echo -e "Code style check passed."
-    exit 0
-  else
-    echo -e "Code style check failed, please run clang-format"
-    echo "$d" |
-        sed -e "s/\(^-.*$\)/`echo -e \"$COLOR_RED\1$COLOR_END\"`/" |
-        sed -e "s/\(^+.*$\)/`echo -e \"$COLOR_GREEN\1$COLOR_END\"`/"
-    exit 1
-  fi
-done
+# Check if there are changes, and failed
+if ! git diff-index --quiet HEAD --; then echo "Code style check failed, please run clang-format (e.g. with tools/fix_style.sh)"; exit 1; fi
