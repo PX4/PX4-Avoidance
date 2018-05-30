3687468c68bfc3f17e677dc434b060c7dfa7cd64
Martina <martina@rivizzigno.it>
2018-05-30 09:12:45 +0200

    check_code_style: do not check only last commit for style errors

--- check_code_format.sh
+++ check_code_format.sh
@@ -5,15 +5,23 @@ COLOR_RED="\033[0;31m"
 COLOR_GREEN="\033[0;32m" 
 STYLE='google'
 
-FORMAT_DIFF=$(git diff -U0 HEAD^ | python ~/clang-format-diff.py -p1 -style=${STYLE})
+FILES_TO_CHECK=$(git diff --name-only master | grep -E ".*\.(cpp|c|h|hpp)"$)
 
-if [ -z "${FORMAT_DIFF}" ]; then
-  echo -e "Code style check passed."
+if [ -z "${FILES_TO_CHECK}" ]; then
+  echo -e "No diff to master."
   exit 0
-else
-  echo -e "Code style check failed, please run clang-format"
-  echo "$FORMAT_DIFF" | 
-  	sed -e "s/\(^-.*$\)/`echo -e \"$COLOR_RED\1$COLOR_END\"`/" |
-  	sed -e "s/\(^+.*$\)/`echo -e \"$COLOR_GREEN\1$COLOR_END\"`/"
-  exit 1
 fi
+
+for f in $FILES_TO_CHECK; do
+  d=$(diff -u "$f" <(clang-format "$f" -style=${STYLE}))
+  if [ -z "$d" ]; then
+    echo -e "Code style check passed."
+    exit 0
+  else
+    echo -e "Code style check failed, please run clang-format"
+    echo "$d" |
+        sed -e "s/\(^-.*$\)/`echo -e \"$COLOR_RED\1$COLOR_END\"`/" |
+        sed -e "s/\(^+.*$\)/`echo -e \"$COLOR_GREEN\1$COLOR_END\"`/"
+    exit 1
+  fi
+done
