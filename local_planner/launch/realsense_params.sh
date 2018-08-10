674ea570b118406a4971a5ab4671cd2562683305
baumanta <baumanta@student.ethz.ch>
2018-08-10 09:19:30 +0200

    Switch off and on autoexposure of realsense automatically. Correct the transforms for the camera_link frame

---
+++ local_planner/launch/realsense_params.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+rosrun dynamic_reconfigure dynparam set /camera_front/realsense2_camera_manager rs435_depth_enable_auto_exposure 0
+rosrun dynamic_reconfigure dynparam set /camera_front/realsense2_camera_manager rs435_depth_enable_auto_exposure 1
+
+rosrun dynamic_reconfigure dynparam set /camera_left/realsense2_camera_manager rs435_depth_enable_auto_exposure 0
+rosrun dynamic_reconfigure dynparam set /camera_left/realsense2_camera_manager rs435_depth_enable_auto_exposure 1
+
+rosrun dynamic_reconfigure dynparam set /camera_right/realsense2_camera_manager rs435_depth_enable_auto_exposure 0
+rosrun dynamic_reconfigure dynparam set /camera_right/realsense2_camera_manager rs435_depth_enable_auto_exposure 1
