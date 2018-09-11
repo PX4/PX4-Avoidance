57fb067998e2669935f7628603e69883f020159f
baumanta <baumanta@student.ethz.ch>
2018-09-11 09:57:04 +0200

    publish memory and cpu info on rostopics

--- local_planner/src/nodes/common.cpp
+++ local_planner/src/nodes/common.cpp
@@ -186,3 +186,15 @@ double getAngularVelocity(double desired_yaw, double curr_yaw) {
 
   return 0.5 * vel;
 }
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    while (!feof(pipe.get())) {
+        if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
+            result += buffer.data();
+    }
+    return result;
+}
