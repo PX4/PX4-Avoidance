26444180f667c7b4dc6e56fa91918b136f13eced
baumanta <baumanta@student.ethz.ch>
2018-10-04 09:17:14 +0200

    Fix in visualization of bounding box

--- local_planner/src/nodes/box.h
+++ local_planner/src/nodes/box.h
@@ -17,14 +17,14 @@ class Box {
   double radius_;
   double zsize_up_;
   double zsize_down_;
-  double zmin_;
-  double zmax_;
 
  private:
   double xmin_;
   double xmax_;
   double ymin_;
   double ymax_;
+  double zmin_;
+  double zmax_;
 };
 
 #endif  // BOX_H
