44ef47f7e95abd8eab83615f4110e6776112e69f
Nico van Duijn <nico@auterion.com>
2019-01-09 16:15:10 +0100

    Add test for elevationAngleFromCartesian
    This commit adds a (somewhat overkill) test for the planner function
    elevationAngleFromCartesian. Corner cases are also included.

--- local_planner/test/test_common.cpp
+++ local_planner/test/test_common.cpp
@@ -72,3 +72,43 @@ TEST(Common, azimuthAnglefromCartesian){
   EXPECT_EQ(138, angle_q4);
   EXPECT_EQ(116, angle_non_zero_origin);
 }
+
+TEST(Common, elevationAnglefromCartesian){
+  //GIVEN:
+  const geometry_msgs::Point point_front = createPoint(0.0d, 1.0d, 0.0d);
+  const geometry_msgs::Point point_up = createPoint(0.0d, 0.0d, 1.0d);
+  const geometry_msgs::Point point_behind = createPoint(0.0d, -1.0d, 0.0d);
+  const geometry_msgs::Point point_down = createPoint(0.0d, 0.0d, -1.0d);
+  const geometry_msgs::Point point_q1 = createPoint(0.0d, 1.0d, 2.0d);
+  const geometry_msgs::Point point_q2 = createPoint(0.0d, -3.0d, 4.0d);
+  const geometry_msgs::Point point_q3 = createPoint(0.0d, -5.0d, -6.0d);
+  const geometry_msgs::Point point_q4 = createPoint(0.0d, 7.0d, -8.0d);
+  const geometry_msgs::Point origin = createPoint(0.0d, 0.0d, 0.0d);
+
+  //WHEN:
+  int angle_front = elevationAnglefromCartesian(point_front, origin);
+  int angle_up = elevationAnglefromCartesian(point_up, origin);
+  int angle_behind = elevationAnglefromCartesian(point_behind, origin);
+  int angle_down = elevationAnglefromCartesian(point_down, origin);
+  int angle_undetermined = elevationAnglefromCartesian(origin, origin);
+  int angle_q1 = elevationAnglefromCartesian(point_q1, origin);
+  int angle_q2 = elevationAnglefromCartesian(point_q2, origin);
+  int angle_q3 = elevationAnglefromCartesian(point_q3, origin);
+  int angle_q4 = elevationAnglefromCartesian(point_q4, origin);
+  int angle_non_zero_origin = elevationAnglefromCartesian(point_q1, point_q2);
+
+  //THEN:
+  EXPECT_EQ(0, angle_front);
+  EXPECT_EQ(0, angle_up);
+  EXPECT_EQ(0, angle_behind);
+  EXPECT_EQ(0, angle_down);
+  EXPECT_EQ(0, angle_undetermined);
+  EXPECT_EQ(63, angle_q1);
+  EXPECT_EQ(53, angle_q2);
+  EXPECT_EQ(-51, angle_q3);
+  EXPECT_EQ(-49, angle_q4);
+  EXPECT_EQ(-27, angle_non_zero_origin);
+
+
+
+}
