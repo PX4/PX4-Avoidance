b36bb176da07f55cb5f495119b9ad5a65b2492f3
Nico van Duijn <nico@auterion.com>
2019-01-24 09:48:13 +0100

    Remove unnecessary SITL stuff from launch file
    Due to many generations of copy-pasting, there were lots of SITL-related
    arguments in the hardware-launch file

--- tools/generate_launchfile.sh
+++ tools/generate_launchfile.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 cat > local_planner/launch/avoidance.launch <<- EOM
 <launch>
-
-
     <arg name="mavros_transformation" default="0" />
-
-    <param name="use_sim_time" value="false" />
-
-    <node pkg="tf" type="static_transform_publisher" name="tf_90_deg"
-          args="0 0 0 \$(arg mavros_transformation) 0 0 world local_origin 10"/>
-
-    <arg name="headless" default="false"/>
     <arg name="ns" default="/"/>
-    <arg name="build" default="px4_sitl_default"/>
     <arg name="fcu_url" default="udp://:14540@localhost:14557"/>
     <arg name="gcs_url" default="" />   <!-- GCS link is provided by SITL -->
     <arg name="tgt_system" default="1" />
     <arg name="tgt_component" default="1" />
-    <arg name="est" default="ekf2"/>
+
+    <!-- Launch static tf publisher -->
+    <node pkg="tf" type="static_transform_publisher" name="tf_90_deg"
+          args="0 0 0 \$(arg mavros_transformation) 0 0 world local_origin 10"/>
 
     <!-- Launch MavROS -->
     <group ns="\$(arg ns)">
