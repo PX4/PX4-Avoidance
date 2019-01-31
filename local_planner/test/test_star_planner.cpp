4a37356a186492ae12bbb31c9a2ee16575ed8e2d
Martina Rivizzigno <martina@rivizzigno.it>
2019-01-31 16:57:06 +0100

    test_star_planner: check for progress towards the goal over multiple iterations instead of actual node positions

--- local_planner/test/test_star_planner.cpp
+++ local_planner/test/test_star_planner.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include "../src/nodes/common.h"
 #include "../src/nodes/star_planner.h"
 #include "../src/nodes/tree_node.h"
 
@@ -12,6 +13,8 @@ class StarPlannerTests : public ::testing::Test {
   float obstacle_min_x = 0.0f;
   float obstacle_max_x = 2.5f;
   float obstacle_y = 2.0f;
+  geometry_msgs::Point goal;
+  geometry_msgs::PoseStamped position;
 
   void SetUp() override {
     ros::Time::init();
@@ -22,19 +25,17 @@ class StarPlannerTests : public ::testing::Test {
     config.n_expanded_nodes_ = 10;
     star_planner.dynamicReconfigureSetStarParams(config, 1);
 
-    geometry_msgs::PoseStamped position;
-    position.pose.position.x = 1.2f;
-    position.pose.position.y = 0.4f;
-    position.pose.position.z = 4.0f;
-    position.pose.orientation.x = 0.0f;
-    position.pose.orientation.y = 0.0f;
-    position.pose.orientation.z = 0.0f;
-    position.pose.orientation.w = 0.0f;
+    position.pose.position.x = 1.2;
+    position.pose.position.y = 0.4;
+    position.pose.position.z = 4.0;
+    position.pose.orientation.x = 0.0;
+    position.pose.orientation.y = 0.0;
+    position.pose.orientation.z = 0.0;
+    position.pose.orientation.w = 0.0;
 
-    geometry_msgs::Point goal;
-    goal.x = 2.0f;
-    goal.y = 14.0f;
-    goal.z = 4.0f;
+    goal.x = 2.0;
+    goal.y = 14.0;
+    goal.z = 4.0;
 
     pcl::PointCloud<pcl::PointXYZ> cloud;
     for (float x = obstacle_min_x; x < obstacle_max_x; x += 0.05f) {
@@ -48,10 +49,10 @@ class StarPlannerTests : public ::testing::Test {
     const std::vector<int> reprojected_points_age;
 
     star_planner.setParams(cost_params);
-    star_planner.setFOV(270.0, 45.0);
+    star_planner.setFOV(270.0f, 45.0f);
     star_planner.setReprojectedPoints(reprojected_points,
                                       reprojected_points_age);
-    star_planner.setPose(position, 0.0);
+    star_planner.setPose(position, 0.0f);
     star_planner.setGoal(goal);
     star_planner.setCloud(cloud);
   }
@@ -59,41 +60,35 @@ class StarPlannerTests : public ::testing::Test {
 };
 
 TEST_F(StarPlannerTests, buildTree) {
-  // GIVEN: a vehicle position, a goal, and an obstacle in between the stright
+  // GIVEN: a vehicle position, a goal, and an obstacle in between the straight
   // line position-goal
 
-  // WHEN: we build the tree
-  star_planner.buildLookAheadTree();
+  float dist_to_goal = 1000.0f;
 
-  // THEN: no cell position should be in the obstacle coordinates
-  std::vector<TreeNode> tree_truth;
-  TreeNode n0(0, 0, Eigen::Vector3f(1.2, 0.4, 4));
-  TreeNode n1(0, 1, Eigen::Vector3f(2.18633, 0.55622, 3.94766));
-  TreeNode n2(1, 2, Eigen::Vector3f(3.17267, 0.71244, 3.89532));
-  TreeNode n3(2, 3, Eigen::Vector3f(4.159, 0.86866, 3.842992));
+  // WHEN: we build the tree for 15 times
+  for (size_t i = 0; i < 15; i++) {
+    star_planner.buildLookAheadTree();
+    for (auto node : star_planner.tree_) {
+      // THEN: we expect each tree node position not to be close to the obstacle
+      Eigen::Vector3f n = node.getPosition();
+      bool node_inside_obstacle =
+          n.x() > obstacle_min_x && n.x() < obstacle_max_x &&
+          n.y() > obstacle_y - 0.1f && n.y() < obstacle_y + 0.1 &&
+          n.z() > 4.0f - obstacle_half_height &&
+          n.z() < 4.0f + obstacle_half_height;
+      EXPECT_FALSE(node_inside_obstacle);
+    }
 
-  TreeNode n4(3, 4, Eigen::Vector3f(5.14534, 1.02488, 3.79065));
-  TreeNode n5(4, 5, Eigen::Vector3f(6.13167, 1.1811, 3.73832));
-  TreeNode n6(5, 6, Eigen::Vector3f(7.11801, 1.33732, 3.68598));
-  TreeNode n7(6, 7, Eigen::Vector3f(8.10434, 1.49354, 3.63364));
-  TreeNode n8(7, 8, Eigen::Vector3f(9.09068, 1.64976, 3.58131));
-  TreeNode n9(8, 9, Eigen::Vector3f(10.07701, 1.80598, 3.52897));
-  TreeNode n10(9, 10, Eigen::Vector3f(11.063347, 1.9622, 3.47664));
+    // THEN: we expect the distance between the vehicle and the goal to shorten
+    // at each iteration
+    float tmp_dist_to_goal =
+        (toEigen(goal) - toEigen(position.pose.position)).norm();
+    ASSERT_LT(tmp_dist_to_goal, dist_to_goal);
+    dist_to_goal = tmp_dist_to_goal;
 
-  tree_truth = {n0, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10};
-
-  for (size_t i = 0, j = 0;
-       i < star_planner.tree_.size(), j < tree_truth.size(); i++, j++) {
-    Eigen::Vector3f n = star_planner.tree_[i].getPosition();
-    Eigen::Vector3f t = tree_truth[j].getPosition();
-    ASSERT_NEAR(t.x(), n.x(), .00001) << "Node number: " << i;
-    ASSERT_NEAR(t.y(), n.y(), .00001) << "Node number: " << i;
-    ASSERT_NEAR(t.z(), n.z(), .00001) << "Node number: " << i;
-    bool node_inside_obstacle =
-        n.x() > obstacle_min_x && n.x() < obstacle_max_x &&
-        n.y() > obstacle_y - 0.1f && n.y() < obstacle_y + 0.1 &&
-        n.z() > 4.0f - obstacle_half_height &&
-        n.z() < 4.0f + obstacle_half_height;
-    EXPECT_FALSE(node_inside_obstacle);
+    // we set the vehicle position to be the first node position after the
+    // origin for the next algorithm iterarion
+    position.pose.position = toPoint(star_planner.tree_[1].getPosition());
+    star_planner.setPose(position, 0.0);
   }
 }
