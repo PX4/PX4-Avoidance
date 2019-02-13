2ee6eab139df944d96ea7d77ae35482b05cce3fb
Martina Rivizzigno <martina@rivizzigno.it>
2019-02-13 11:57:18 +0100

    add doxygen comments to remaining header files

--- local_planner/include/local_planner/tree_node.h
+++ local_planner/include/local_planner/tree_node.h
@@ -22,7 +22,17 @@ class TreeNode {
   TreeNode(int from, int d, const Eigen::Vector3f& pos);
   ~TreeNode();
 
+  /**
+  * @brief     setter method for heuristic and cost of a tree node
+  * @param[in] h, heuristic
+  * @param[in] c, cost
+  **/
   void setCosts(float h, float c);
+
+  /**
+  * @brief     getter method for tree node position
+  * @returns   node position in 3D cartesian coordinates
+  **/
   Eigen::Vector3f getPosition();
 };
 }
