77c3dc0738843f0341b945e5569ac62d02afc2fb
Nico van Duijn <nicovanduijn@users.noreply.github.com>
2019-02-27 11:23:45 +0100

    Change verbosity of ROS console during tests (#237)
    This makes for a nicer output when running the tests.

--- local_planner/test/main.cpp
+++ local_planner/test/main.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
+#include <ros/console.h>
 
 int main(int argc, char **argv) {
+  ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME,
+                                 ros::console::levels::Error);
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
