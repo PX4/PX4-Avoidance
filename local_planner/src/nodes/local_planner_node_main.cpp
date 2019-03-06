7864c41cf8ee90d64046f427b2f64eb21f331157
baumanta <baumanta@student.ethz.ch>
2019-03-06 14:22:54 +0100

    remove timout state at start

--- local_planner/src/nodes/local_planner_node_main.cpp
+++ local_planner/src/nodes/local_planner_node_main.cpp
@@ -61,8 +61,8 @@ int main(int argc, char** argv) {
         ROS_WARN("\033[1;33m Pointcloud timeout: Aborting \n \033[0m");
       }
     } else {
-      if (Node.never_run_ || (since_last_cloud > pointcloud_timeout_hover &&
-                              since_start > pointcloud_timeout_hover)) {
+      if (since_last_cloud > pointcloud_timeout_hover &&
+          since_start > pointcloud_timeout_hover) {
         if (Node.position_received_) {
           hover = true;
           Node.status_msg_.state = (int)MAV_STATE::MAV_STATE_CRITICAL;
