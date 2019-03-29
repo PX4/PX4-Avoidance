488371ee82f845a234f4147e334ffa3da543f295
JaeyoungLim <gokoreas@gmail.com>
2019-03-29 21:15:35 +0100

    main function cleanup

--- local_planner/src/nodes/local_planner_node_main.cpp
+++ local_planner/src/nodes/local_planner_node_main.cpp
@@ -1,12 +1,8 @@
-#include "local_planner/local_planner.h"
-#include "local_planner/common.h"
 #include "local_planner/local_planner_node.h"
-#include "local_planner/waypoint_generator.h"
 
-#include <boost/algorithm/string.hpp>
+using namespace avoidance;
 
 int main(int argc, char** argv) {
-  using namespace avoidance;
   ros::init(argc, argv, "local_planner_node");
   ros::NodeHandle nh("~");
   ros::NodeHandle nh_private("");
