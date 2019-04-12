sunzj/avoidance

remove take_off_position, use minimum takeoff altitude from Firmware to set the starting height

local_planner/src/nodes/local_planner.cpp
@@ -22,7 +22,6 @@ void LocalPlanner::setPose(const Eigen::Vector3f &pos,
   star_planner_->setPose(position_, curr_yaw_fcu_frame_);
 
   if (!currently_armed_ && !disable_rise_to_goal_altitude_) {
-    take_off_pose_ = position_;
     reach_altitude_ = false;
   }
 }
@@ -157,7 +156,7 @@ void LocalPlanner::determineStrategy() {
   if (!reach_altitude_) {
     // use half of the goal altitude as an acceptance altitude as done in
     // the Firmware
-    starting_height_ = std::max(goal_.z() / 2.0f, take_off_pose_.z() + 1.0f);
+    starting_height_ = std::max(goal_.z() / 2.0f, model_params_.min_takeoff_alt);
     ROS_INFO("\033[1;35m[OA] Reach height (%f) first: Go fast\n \033[0m",
              starting_height_);
     waypoint_type_ = reachHeight;
@@ -484,7 +483,6 @@ avoidanceOutput LocalPlanner::getAvoidanceOutput() {
   out.back_off_start_point = back_off_start_point_;
   out.min_dist_backoff = min_dist_backoff_;
 
-  out.take_off_pose = take_off_pose_;
   out.starting_height = starting_height_;
 
   out.costmap_direction_e = costmap_direction_e_;

PX4/PX4-Avoidance

local_planner/src/nodes/local_planner.cpp
@@ -24,7 +24,6 @@ void LocalPlanner::setPose(const Eigen::Vector3f& pos,
   star_planner_->setPose(position_, curr_yaw_histogram_frame_deg_);
 
   if (!currently_armed_ && !disable_rise_to_goal_altitude_) {
-    take_off_pose_ = position_;
     reach_altitude_ = false;
   }
 }
@@ -143,7 +142,7 @@ void LocalPlanner::determineStrategy() {
   if (!reach_altitude_) {
     // use half of the goal altitude as an acceptance altitude as done in
     // the Firmware
-    starting_height_ = std::max(goal_.z() / 2.0f, take_off_pose_.z() + 1.0f);
+    starting_height_ = std::max(goal_.z() / 2.0f, px4_.param_mis_takeoff_alt);
     ROS_INFO("\033[1;35m[OA] Reach height (%f) first: Go fast\n \033[0m",
              starting_height_);
     waypoint_type_ = reachHeight;
@@ -340,7 +339,6 @@ avoidanceOutput LocalPlanner::getAvoidanceOutput() const {
   out.cruise_velocity = px4_.param_mpc_xy_cruise;
   out.last_path_time = last_path_time_;
 
-  out.take_off_pose = take_off_pose_;
   out.starting_height = starting_height_;
 
   out.path_node_positions = star_planner_->path_node_positions_;