b4c835caaf72022cde7e06b9973e0700a1522611
baumanta <tanja@auterion.com>
2019-04-17 11:19:53 +0200

    discourage height change

--- local_planner/include/local_planner/cost_parameters.h
+++ local_planner/include/local_planner/cost_parameters.h
@@ -6,7 +6,7 @@ struct costParameters {
   float heading_cost_param = 0.5f;
   float goal_cost_param = 3.f;
   float smooth_cost_param = 1.5f;
-  float height_change_cost_param = 4.f;
-  float height_change_cost_param_adapted = 4.f;
+  float height_change_cost_param = 20.f;
+  float height_change_cost_param_adapted = 20.f;
 };
 }
