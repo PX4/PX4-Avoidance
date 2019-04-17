c26b4602bd4ee128227d8c0ef87b7fe9cc09ab99
baumanta <tanja@auterion.com>
2019-04-17 16:41:25 +0200

    fix no position received debug output

--- local_planner/src/nodes/local_planner_node_main.cpp
+++ local_planner/src/nodes/local_planner_node_main.cpp
@@ -14,27 +14,7 @@ int main(int argc, char** argv) {
   std::thread worker(&LocalPlannerNode::threadFunction, &Node);
 
   std::thread worker_params(&LocalPlannerNode::checkPx4Parameters, &Node);
-  ros::Duration timeout_termination =
-      ros::Duration(Node.local_planner_->timeout_termination_);
-  ros::Time start_query_position = ros::Time::now();
-  bool sent_error = false;
-  ros::Duration since_query = ros::Time::now() - start_query_position;
-  if (since_query > timeout_termination && !sent_error) {
-    // clang-format off
-    	  ROS_WARN("\033[1;33m Planner abort: missing required data \n \033[0m");
-    	  ROS_WARN("----------------------------- Debugging Info -----------------------------");
-    	  ROS_WARN("Local planner has not received a position from FCU, check the following: ");
-    	  ROS_WARN("1. Check cables connecting PX4 autopilot with onboard computer");
-    	  ROS_WARN("2. Set PX4 parameter MAV_1_MODE to onbard or external vision");
-    	  ROS_WARN("3. Set correct fcu_url in local_planner launch file:");
-    	  ROS_WARN("   Example direct connection to serial port: /dev/ttyUSB0:921600");
-    	  ROS_WARN("   Example connection over mavlink router: udp://:14540@localhost:14557");
-    	  ROS_WARN("--------------------------------------------------------------------------");
-    // clang-format on
-    Node.status_msg_.state = (int)MAV_STATE::MAV_STATE_FLIGHT_TERMINATION;
-    Node.publishSystemStatus();
-    sent_error = true;
-  }
+
   worker.join();
   worker_params.join();
 
