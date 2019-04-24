f5b15c56be29fc0d2f3771dfb5f6a3b3e1e1e499
Julian Kent <jkflying@gmail.com>
2019-04-24 18:10:23 +0200

    Always run the goal update

--- local_planner/src/nodes/local_planner_node.cpp
+++ local_planner/src/nodes/local_planner_node.cpp
@@ -238,7 +238,7 @@ void LocalPlannerNode::updatePlannerInfo() {
     local_planner_->setGoal(toEigen(goal_msg_.pose.position));
     new_goal_ = false;
   }
-  if (!goal_set_to_straight_){
+  {
 	goal_msg_.pose.position.x = newest_pose_.pose.position.x + 10;
 	goal_msg_.pose.position.y = newest_pose_.pose.position.y;
 	goal_msg_.pose.position.z = newest_pose_.pose.position.z;
