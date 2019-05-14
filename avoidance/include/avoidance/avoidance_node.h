5b56c3e30c6e76eee79a59a9f127f0672ea2f120
JaeyoungLim <gokoreas@gmail.com>
2019-05-14 22:44:56 +0200

    Add status loop

--- avoidance/include/avoidance/avoidance_node.h
+++ avoidance/include/avoidance/avoidance_node.h
@@ -18,13 +18,15 @@ class AvoidanceNode {
   ros::NodeHandle nh_;
   ros::NodeHandle nh_private_;
 
-  ros::Timer cmdloop_timer_;
-  ros::CallbackQueue cmdloop_queue_;
+  ros::Timer cmdloop_timer_, statusloop_timer_;
+  ros::CallbackQueue cmdloop_queue_, statusloop_queue_;
   std::unique_ptr<ros::AsyncSpinner> cmdloop_spinner_;
+  std::unique_ptr<ros::AsyncSpinner> statusloop_spinner_;
 
-  double spin_dt_;
+  double cmdloop_dt_, statusloop_dt_;
 
   void cmdLoopCallback(const ros::TimerEvent& event);
+  void statusLoopCallback(const ros::TimerEvent& event);
   
 };
 }
--- avoidance/src/avoidance_node.cpp
+++ avoidance/src/avoidance_node.cpp
@@ -4,13 +4,20 @@ namespace avoidance {
 
 AvoidanceNode::AvoidanceNode(const ros::NodeHandle& nh,
                                    const ros::NodeHandle& nh_private)
-    : nh_(nh), nh_private_(nh_private), spin_dt_(0.1) {
+    : nh_(nh), nh_private_(nh_private), cmdloop_dt_(0.1), statusloop_dt_(0.2) {
 
-  ros::TimerOptions timer_options(
-      ros::Duration(spin_dt_),
+  ros::TimerOptions cmdlooptimer_options(
+      ros::Duration(cmdloop_dt_),
       boost::bind(&AvoidanceNode::cmdLoopCallback, this, _1),
       &cmdloop_queue_);
-  cmdloop_timer_ = nh_.createTimer(timer_options);
+  cmdloop_timer_ = nh_.createTimer(cmdlooptimer_options);
+
+  ros::TimerOptions statuslooptimer_options(
+      ros::Duration(statusloop_dt_),
+      boost::bind(&AvoidanceNode::statusLoopCallback, this, _1),
+      &statusloop_queue_);
+  statusloop_timer_ = nh_.createTimer(statuslooptimer_options);
+
 
   cmdloop_spinner_.reset(new ros::AsyncSpinner(1, &cmdloop_queue_));
   cmdloop_spinner_->start();
@@ -23,5 +30,8 @@ void AvoidanceNode::cmdLoopCallback(const ros::TimerEvent& event){
 
 }
 
+void AvoidanceNode::statusLoopCallback(const ros::TimerEvent& event){
+
+}
 
 }
