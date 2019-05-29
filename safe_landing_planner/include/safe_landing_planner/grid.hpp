mrivi/avoidance

first commit lsd

landing_site_detection/include/landing_site_detection/grid.hpp
@@ -1,0 +1,78 @@
+#pragma once
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+namespace landing_site_detection {
+
+class Grid {
+public:
+  Grid(const float grid_size, const float cell_size) :
+    grid_size_(grid_size),
+    cell_size_(cell_size),
+    mean_(static_cast<int>(std::ceil(grid_size/cell_size)), static_cast<int>(std::ceil(grid_size/cell_size))),
+    variance_(static_cast<int>(std::ceil(grid_size/cell_size)), static_cast<int>(std::ceil(grid_size/cell_size))),
+    counter_(static_cast<int>(std::ceil(grid_size/cell_size)), static_cast<int>(std::ceil(grid_size/cell_size))),
+    land_(static_cast<int>(std::ceil(grid_size/cell_size)), static_cast<int>(std::ceil(grid_size/cell_size)))
+    {
+      reset();
+    }
+  ~Grid() = default;
+
+  void reset() {
+    mean_.fill(0.f);
+    variance_.fill(0.f);
+    counter_.fill(0);
+    land_.fill(0);
+  }
+
+  void resize() {
+    mean_.resize(static_cast<int>(std::ceil(grid_size_/cell_size_)), static_cast<int>(std::ceil(grid_size_/cell_size_)));
+    variance_.resize(static_cast<int>(std::ceil(grid_size_/cell_size_)), static_cast<int>(std::ceil(grid_size_/cell_size_)));
+    counter_.resize(static_cast<int>(std::ceil(grid_size_/cell_size_)), static_cast<int>(std::ceil(grid_size_/cell_size_)));
+    land_.resize(static_cast<int>(std::ceil(grid_size_/cell_size_)), static_cast<int>(std::ceil(grid_size_/cell_size_)));
+  }
+
+  void setMean(Eigen::Vector2i &idx, float value) {
+    mean_(idx.x(), idx.y()) = value;
+  }
+  void setVariance(Eigen::Vector2i &idx, float value) {
+    variance_(idx.x(), idx.y()) = value;
+  }
+  void increaseCounter(Eigen::Vector2i &idx) {
+    counter_(idx.x(), idx.y()) = counter_(idx.x(), idx.y()) + 1;
+  }
+
+  Eigen::MatrixXf getMean() const { return mean_;}
+  Eigen::MatrixXf getVariance() const { return variance_;}
+  Eigen::MatrixXi getCounter() const { return counter_;}
+
+  float getMean(Eigen::Vector2i &idx) {return mean_(idx.x(), idx.y());}
+  float getVariance(Eigen::Vector2i &idx) {return variance_(idx.x(), idx.y());}
+  int getCounter(Eigen::Vector2i &idx) {return counter_(idx.x(), idx.y()); }
+
+  void setFilterLimits(Eigen::Vector3f &pos) {
+    corner_min_.x() = pos.x() - grid_size_ / 2.f;
+    corner_min_.y() = pos.y() - grid_size_ / 2.f;
+    corner_max_.x() = pos.x() + grid_size_ / 2.f;
+    corner_max_.y() = pos.y() + grid_size_ / 2.f;
+  }
+
+  void getGridLimits(Eigen::Vector2f &min, Eigen::Vector2f &max) const {
+    min = corner_min_;
+    max = corner_max_;
+  }
+
+  float grid_size_;
+  float cell_size_;
+  Eigen::MatrixXi land_;
+  Eigen::MatrixXf mean_;
+
+private:
+  Eigen::Vector2f corner_min_;
+  Eigen::Vector2f corner_max_;
+  Eigen::MatrixXf variance_;
+  Eigen::MatrixXi counter_;
+};
+
+}

landing_site_detection/include/landing_site_detection/waypoint_generator_node.hpp
@@ -1,0 +1,190 @@
+#pragma once
+#include <ros/ros.h>
+#include <ros/time.h>
+#include <ros/callback_queue.h>
+
+#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/Int64MultiArray.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/Trajectory.h>
+#include <mavros_msgs/WaypointList.h>
+#include <mavros_msgs/State.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+
+
+#include <dynamic_reconfigure/server.h>
+#include <landing_site_detection/WaypointGeneratorNodeConfig.h>
+#include <landing_site_detection/LSDGridMsg.h>
+#include "grid.hpp"
+
+
+#include <Eigen/Dense>
+
+
+
+namespace landing_site_detection {
+
+  const std::vector<Eigen::Vector2f> exploration_pattern = {Eigen::Vector2f(1.f, 0.f), Eigen::Vector2f(1.f, 1.f),
+            Eigen::Vector2f(0.f, 1.f), Eigen::Vector2f(-1.f, 1.f), Eigen::Vector2f(-1.f, 0.f), Eigen::Vector2f(-1.f, -1.f),
+          Eigen::Vector2f(0.f, -1.f), Eigen::Vector2f(1.f, -1.f) };
+
+  enum class LSDState {
+    goTo,
+    loiter,
+    land,
+    altitudeChange,
+  };
+
+  class WaypointGeneratorNode {
+
+  public:
+    WaypointGeneratorNode(const ros::NodeHandle& nh);
+    ~WaypointGeneratorNode() = default;
+
+    /**
+    * @brief spins node
+    **/
+    void startNode();
+
+  private:
+    ros::NodeHandle nh_;
+
+    ros::Timer cmdloop_timer_;
+    ros::CallbackQueue cmdloop_queue_;
+    std::unique_ptr<ros::AsyncSpinner> cmdloop_spinner_;
+
+    double spin_dt_ = 0.1;
+    float yaw_setpoint_ = NAN;
+    float yaw_speed_setpoint_ = NAN;
+    float loiter_yaw_ = NAN;
+    float yaw_ = NAN;
+    float beta_ = 0.9f;
+    float landing_radius_ = 2.f;
+    float can_land_thr_= 0.4f;
+    float loiter_height_ = 4.f;
+    float factor_exploration_ = 1.f;
+    bool grid_received_ = false;
+    bool is_land_waypoint_ = false;
+    bool decision_taken_ = false;
+    bool can_land_ = true;
+    bool in_land_vertical_range_ = false;
+    bool is_within_landing_radius_ = false;
+    bool update_smoothing_size_ = false;
+    bool explorarion_is_active_ = false;
+    int smoothing_land_cell_ = 2;
+    int start_seq_landing_decision_ = 0;
+    int grid_lsd_seq_ = 0;
+    int n_explored_pattern_ = -1;
+
+    ros::Subscriber pose_sub_;
+    ros::Subscriber trajectory_sub_;
+    ros::Subscriber mission_sub_;
+
+    ros::Subscriber grid_sub_;
+    ros::Subscriber pos_index_sub_;
+    ros::Subscriber state_sub_;
+
+    ros::Publisher trajectory_pub_;
+    ros::Publisher land_hysteresis_pub_;
+    ros::Publisher marker_goal_pub_;
+
+    Eigen::Vector3f position_ = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector3f goal_ = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector3f goal_visualization_ = Eigen::Vector3f::Zero();
+    Eigen::Vector3f velocity_setpoint_ = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector3f loiter_position_ = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector3f exploration_anchor_ = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector3f vel_sp = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector2i pos_index_ = Eigen::Vector2i(NAN, NAN);
+    Eigen::MatrixXf mean_ = Eigen::MatrixXf(40, 40);
+    Eigen::MatrixXi land_ = Eigen::MatrixXi(40, 40);
+
+    std::vector<float> can_land_hysteresis_;
+    Grid grid_lsd_ = Grid(10.f, 1.f);
+    LSDState lsd_state_ = LSDState::goTo;
+    LSDState prev_lsd_state_ = LSDState::goTo;
+
+    dynamic_reconfigure::Server<landing_site_detection::WaypointGeneratorNodeConfig> server_;
+
+    /**
+    * @brief main loop callback
+    * @param[in] event, event timing information
+    **/
+    void cmdLoopCallback(const ros::TimerEvent& event);
+
+    /**
+    * @brief     sets parameters from ROS parameter server
+    * @param     config, struct containing all the parameters
+    * @param     level, bitmask to group together reconfigurable parameters
+    **/
+    void dynamicReconfigureCallback(landing_site_detection::WaypointGeneratorNodeConfig& config, uint32_t level);
+
+    /**
+    * @brif callback for vehicle position and orientation
+    * @param[in] msg, pose message coming fro the FCU
+    **/
+    void positionCallback(const geometry_msgs::PoseStamped &msg);
+
+    /**
+    * @brief     callaback for setting the goal from the FCU Mission Waypoints
+    * @param[in] msg, current and next position goals
+    **/
+    void trajectoryCallback(const mavros_msgs::Trajectory& msg);
+
+    /**
+    * @brief     callaback with the list of FCU Mission Items
+    * @param[in] msg, list of mission items
+    **/
+    void missionCallback(const mavros_msgs::WaypointList& msg);
+
+    /**
+    * @brief     callaback with the grid calculated by the landing_site_detection
+    * @param[in] msg, grid
+    **/
+    void gridCallback(const landing_site_detection::LSDGridMsg &msg);
+
+    /**
+    * @brief     callaback with the vehicle state
+    * @param[in] msg, FCU vehicle state
+    **/
+    void stateCallback(const mavros_msgs::State &msg);
+
+    /**
+    * @brief     computes the setpoints to be sent to the FCU
+    **/
+    void calculateWaypoint();
+
+    /**
+    * @brief     decides if the desired setpoints received from the FCU should be overwritten
+    *            or sent back because no intervention is needed
+    * @returns   true, overwtite setpoints
+    **/
+    void updateLSDState();
+
+    /**
+    * @brief     publishes the computed waypoints to the FCU
+    * @param[in] pos_sp, position setpoint
+    * @param[in] vel_sp, velocity setpoint
+    * @param[in] yaw_sp, yaw setpoint
+    * @param[in] yaw_speed_sp, yaw speed setpoint
+    **/
+    void publishTrajectorySetpoints(const Eigen::Vector3f &pos_sp, const Eigen::Vector3f &vel_sp, float yaw_sp, float yaw_speed_sp);
+
+    /**
+    * @brief     fills unused waypoints with NANs
+    * @param[in] point, unused waypoints
+    **/
+    void fillUnusedTrajectorySetpoints(mavros_msgs::PositionTarget& point);
+
+    /**
+    * @brief     visualize landing area decision grid in Rviz
+    **/
+    void landingAreaVisualization();
+
+    /**
+    * @brief     visualize goal in Rviz
+    **/
+    void goalVisualization();
+  };
+}

landing_site_detection/test/test_grid.cpp
@@ -1,0 +1,37 @@
+#include <gtest/gtest.h>
+
+#include "../include/landing_site_detection/grid.hpp"
+
+
+using namespace landing_site_detection;
+
+
+TEST(GridTest, gridCellSize) {
+
+  Grid grid = Grid(6.f, 2.f);
+  Eigen::Vector3f pos(1.2f, 3.4f, 2.f);
+  grid.setFilterLimits(pos);
+  Eigen::Vector2f limit_min, limit_max;
+  grid.getGridLimits(limit_min, limit_max);
+
+  EXPECT_FLOAT_EQ(-1.8f, limit_min.x());
+  EXPECT_FLOAT_EQ(0.4f, limit_min.y());
+  EXPECT_FLOAT_EQ(4.2f, limit_max.x());
+  EXPECT_FLOAT_EQ(6.4f, limit_max.y());
+
+}
+
+TEST(GridTest, gridCellSize2) {
+
+  Grid grid = Grid(12.f, 3.f);
+  Eigen::Vector3f pos(-3.2f, 5.4f, 2.f);
+  grid.setFilterLimits(pos);
+  Eigen::Vector2f limit_min, limit_max;
+  grid.getGridLimits(limit_min, limit_max);
+
+  EXPECT_FLOAT_EQ(-9.2f, limit_min.x());
+  EXPECT_FLOAT_EQ(-0.6f, limit_min.y());
+  EXPECT_FLOAT_EQ(2.8f, limit_max.x());
+  EXPECT_FLOAT_EQ(11.4f, limit_max.y());
+
+}

PX4/PX4-Avoidance

safe_landing_planner/include/safe_landing_planner/grid.hpp
@@ -1,0 +1,78 @@
+#pragma once
+
+#include <Eigen/Core>
+#include <Eigen/Dense>
+
+namespace avoidance {
+
+class Grid {
+public:
+  Grid(const float grid_size, const float cell_size) :
+    grid_size_(grid_size),
+    cell_size_(cell_size),
+    mean_(static_cast<int>(std::ceil(grid_size/cell_size)), static_cast<int>(std::ceil(grid_size/cell_size))),
+    variance_(static_cast<int>(std::ceil(grid_size/cell_size)), static_cast<int>(std::ceil(grid_size/cell_size))),
+    counter_(static_cast<int>(std::ceil(grid_size/cell_size)), static_cast<int>(std::ceil(grid_size/cell_size))),
+    land_(static_cast<int>(std::ceil(grid_size/cell_size)), static_cast<int>(std::ceil(grid_size/cell_size)))
+    {
+      reset();
+    }
+  ~Grid() = default;
+
+  void reset() {
+    mean_.fill(0.f);
+    variance_.fill(0.f);
+    counter_.fill(0);
+    land_.fill(0);
+  }
+
+  void resize() {
+    mean_.resize(static_cast<int>(std::ceil(grid_size_/cell_size_)), static_cast<int>(std::ceil(grid_size_/cell_size_)));
+    variance_.resize(static_cast<int>(std::ceil(grid_size_/cell_size_)), static_cast<int>(std::ceil(grid_size_/cell_size_)));
+    counter_.resize(static_cast<int>(std::ceil(grid_size_/cell_size_)), static_cast<int>(std::ceil(grid_size_/cell_size_)));
+    land_.resize(static_cast<int>(std::ceil(grid_size_/cell_size_)), static_cast<int>(std::ceil(grid_size_/cell_size_)));
+  }
+
+  void setMean(Eigen::Vector2i &idx, float value) {
+    mean_(idx.x(), idx.y()) = value;
+  }
+  void setVariance(Eigen::Vector2i &idx, float value) {
+    variance_(idx.x(), idx.y()) = value;
+  }
+  void increaseCounter(Eigen::Vector2i &idx) {
+    counter_(idx.x(), idx.y()) = counter_(idx.x(), idx.y()) + 1;
+  }
+
+  Eigen::MatrixXf getMean() const { return mean_;}
+  Eigen::MatrixXf getVariance() const { return variance_;}
+  Eigen::MatrixXi getCounter() const { return counter_;}
+
+  float getMean(Eigen::Vector2i &idx) {return mean_(idx.x(), idx.y());}
+  float getVariance(Eigen::Vector2i &idx) {return variance_(idx.x(), idx.y());}
+  int getCounter(Eigen::Vector2i &idx) {return counter_(idx.x(), idx.y()); }
+
+  void setFilterLimits(Eigen::Vector3f &pos) {
+    corner_min_.x() = pos.x() - grid_size_ / 2.f;
+    corner_min_.y() = pos.y() - grid_size_ / 2.f;
+    corner_max_.x() = pos.x() + grid_size_ / 2.f;
+    corner_max_.y() = pos.y() + grid_size_ / 2.f;
+  }
+
+  void getGridLimits(Eigen::Vector2f &min, Eigen::Vector2f &max) const {
+    min = corner_min_;
+    max = corner_max_;
+  }
+
+  float grid_size_;
+  float cell_size_;
+  Eigen::MatrixXi land_;
+  Eigen::MatrixXf mean_;
+
+private:
+  Eigen::Vector2f corner_min_;
+  Eigen::Vector2f corner_max_;
+  Eigen::MatrixXf variance_;
+  Eigen::MatrixXi counter_;
+};
+
+}

safe_landing_planner/include/safe_landing_planner/waypoint_generator_node.hpp
@@ -1,0 +1,187 @@
+#pragma once
+#include <ros/ros.h>
+#include <ros/time.h>
+
+#include <std_msgs/Float64MultiArray.h>
+#include <std_msgs/Int64MultiArray.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/Trajectory.h>
+#include <mavros_msgs/WaypointList.h>
+#include <mavros_msgs/State.h>
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
+
+
+#include <dynamic_reconfigure/server.h>
+#include <safe_landing_planner/WaypointGeneratorNodeConfig.h>
+#include <safe_landing_planner/LSDGridMsg.h>
+#include "grid.hpp"
+
+
+#include <Eigen/Dense>
+
+namespace avoidance {
+
+  const std::vector<Eigen::Vector2f> exploration_pattern = {Eigen::Vector2f(1.f, 0.f), Eigen::Vector2f(1.f, 1.f),
+            Eigen::Vector2f(0.f, 1.f), Eigen::Vector2f(-1.f, 1.f), Eigen::Vector2f(-1.f, 0.f), Eigen::Vector2f(-1.f, -1.f),
+          Eigen::Vector2f(0.f, -1.f), Eigen::Vector2f(1.f, -1.f) };
+
+  enum class LSDState {
+    goTo,
+    loiter,
+    land,
+    altitudeChange,
+  };
+
+  class WaypointGeneratorNode {
+
+  public:
+    WaypointGeneratorNode(const ros::NodeHandle& nh);
+    ~WaypointGeneratorNode() = default;
+
+    /**
+    * @brief spins node
+    **/
+    void startNode();
+
+  private:
+    ros::NodeHandle nh_;
+
+    ros::Timer cmdloop_timer_;
+    std::unique_ptr<ros::AsyncSpinner> cmdloop_spinner_;
+
+    double spin_dt_ = 0.1;
+    float yaw_setpoint_ = NAN;
+    float yaw_speed_setpoint_ = NAN;
+    float loiter_yaw_ = NAN;
+    float yaw_ = NAN;
+    float beta_ = 0.9f;
+    float landing_radius_ = 2.f;
+    float can_land_thr_= 0.4f;
+    float loiter_height_ = 4.f;
+    float factor_exploration_ = 1.f;
+    float vertical_range_error_ = 1.f;
+    bool grid_received_ = false;
+    bool is_land_waypoint_ = false;
+    bool decision_taken_ = false;
+    bool can_land_ = true;
+    bool in_land_vertical_range_ = false;
+    bool is_within_landing_radius_ = false;
+    bool update_smoothing_size_ = false;
+    bool explorarion_is_active_ = false;
+    int smoothing_land_cell_ = 2;
+    int start_seq_landing_decision_ = 0;
+    int grid_lsd_seq_ = 0;
+    int n_explored_pattern_ = -1;
+
+    ros::Subscriber pose_sub_;
+    ros::Subscriber trajectory_sub_;
+    ros::Subscriber mission_sub_;
+
+    ros::Subscriber grid_sub_;
+    ros::Subscriber pos_index_sub_;
+    ros::Subscriber state_sub_;
+
+    ros::Publisher trajectory_pub_;
+    ros::Publisher land_hysteresis_pub_;
+    ros::Publisher marker_goal_pub_;
+
+    Eigen::Vector3f position_ = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector3f goal_ = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector3f goal_visualization_ = Eigen::Vector3f::Zero();
+    Eigen::Vector3f velocity_setpoint_ = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector3f loiter_position_ = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector3f exploration_anchor_ = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector3f vel_sp = Eigen::Vector3f(NAN, NAN, NAN);
+    Eigen::Vector2i pos_index_ = Eigen::Vector2i(NAN, NAN);
+    Eigen::MatrixXf mean_ = Eigen::MatrixXf(40, 40);
+    Eigen::MatrixXi land_ = Eigen::MatrixXi(40, 40);
+
+    std::vector<float> can_land_hysteresis_;
+    Grid grid_lsd_ = Grid(10.f, 1.f);
+    LSDState lsd_state_ = LSDState::goTo;
+    LSDState prev_lsd_state_ = LSDState::goTo;
+
+    dynamic_reconfigure::Server<safe_landing_planner::WaypointGeneratorNodeConfig> server_;
+
+    /**
+    * @brief main loop callback
+    * @param[in] event, event timing information
+    **/
+    void cmdLoopCallback(const ros::TimerEvent& event);
+
+    /**
+    * @brief     sets parameters from ROS parameter server
+    * @param     config, struct containing all the parameters
+    * @param     level, bitmask to group together reconfigurable parameters
+    **/
+    void dynamicReconfigureCallback(safe_landing_planner::WaypointGeneratorNodeConfig& config, uint32_t level);
+
+    /**
+    * @brif callback for vehicle position and orientation
+    * @param[in] msg, pose message coming fro the FCU
+    **/
+    void positionCallback(const geometry_msgs::PoseStamped &msg);
+
+    /**
+    * @brief     callaback for setting the goal from the FCU Mission Waypoints
+    * @param[in] msg, current and next position goals
+    **/
+    void trajectoryCallback(const mavros_msgs::Trajectory& msg);
+
+    /**
+    * @brief     callaback with the list of FCU Mission Items
+    * @param[in] msg, list of mission items
+    **/
+    void missionCallback(const mavros_msgs::WaypointList& msg);
+
+    /**
+    * @brief     callaback with the grid calculated by the safe_landing_planner
+    * @param[in] msg, grid
+    **/
+    void gridCallback(const safe_landing_planner::LSDGridMsg &msg);
+
+    /**
+    * @brief     callaback with the vehicle state
+    * @param[in] msg, FCU vehicle state
+    **/
+    void stateCallback(const mavros_msgs::State &msg);
+
+    /**
+    * @brief     computes the setpoints to be sent to the FCU
+    **/
+    void calculateWaypoint();
+
+    /**
+    * @brief     decides if the desired setpoints received from the FCU should be overwritten
+    *            or sent back because no intervention is needed
+    * @returns   true, overwtite setpoints
+    **/
+    void updateLSDState();
+
+    /**
+    * @brief     publishes the computed waypoints to the FCU
+    * @param[in] pos_sp, position setpoint
+    * @param[in] vel_sp, velocity setpoint
+    * @param[in] yaw_sp, yaw setpoint
+    * @param[in] yaw_speed_sp, yaw speed setpoint
+    **/
+    void publishTrajectorySetpoints(const Eigen::Vector3f &pos_sp, const Eigen::Vector3f &vel_sp, float yaw_sp, float yaw_speed_sp);
+
+    /**
+    * @brief     fills unused waypoints with NANs
+    * @param[in] point, unused waypoints
+    **/
+    void fillUnusedTrajectorySetpoints(mavros_msgs::PositionTarget& point);
+
+    /**
+    * @brief     visualize landing area decision grid in Rviz
+    **/
+    void landingAreaVisualization();
+
+    /**
+    * @brief     visualize goal in Rviz
+    **/
+    void goalVisualization();
+  };
+}

safe_landing_planner/test/test_grid.cpp
@@ -1,0 +1,31 @@
+#include <gtest/gtest.h>
+
+#include "../include/safe_landing_planner/grid.hpp"
+
+using namespace avoidance;
+
+TEST(GridTest, gridCellSize) {
+  Grid grid = Grid(6.f, 2.f);
+  Eigen::Vector3f pos(1.2f, 3.4f, 2.f);
+  grid.setFilterLimits(pos);
+  Eigen::Vector2f limit_min, limit_max;
+  grid.getGridLimits(limit_min, limit_max);
+
+  EXPECT_FLOAT_EQ(-1.8f, limit_min.x());
+  EXPECT_FLOAT_EQ(0.4f, limit_min.y());
+  EXPECT_FLOAT_EQ(4.2f, limit_max.x());
+  EXPECT_FLOAT_EQ(6.4f, limit_max.y());
+}
+
+TEST(GridTest, gridCellSize2) {
+  Grid grid = Grid(12.f, 3.f);
+  Eigen::Vector3f pos(-3.2f, 5.4f, 2.f);
+  grid.setFilterLimits(pos);
+  Eigen::Vector2f limit_min, limit_max;
+  grid.getGridLimits(limit_min, limit_max);
+
+  EXPECT_FLOAT_EQ(-9.2f, limit_min.x());
+  EXPECT_FLOAT_EQ(-0.6f, limit_min.y());
+  EXPECT_FLOAT_EQ(2.8f, limit_max.x());
+  EXPECT_FLOAT_EQ(11.4f, limit_max.y());
+}