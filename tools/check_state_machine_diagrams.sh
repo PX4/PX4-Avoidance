8c41b1731d4ca4c55a4b6abc7c4da2483cb20e8b
Julian Kent <julian@auterion.com>
2019-06-25 13:43:07 +0200

    Add state machine diagram updates to CI

---
+++ tools/check_state_machine_diagrams.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Generate the existing state machine diagrams again
+python generate_flow_diagram.py ../avoidance/test/test_usm.cpp
+python generate_flow_diagram.py ../safe_landing_planner/src/nodes/waypoint_generator_node.cpp
+
+# Print the diff with the remote branch (empty if no diff)
+git --no-pager diff -U0 --color
+
+# Check if there are changes, and failed
+if ! git diff-index --quiet HEAD --; then echo "State machine diagram generation not up to date, run tools/check_state_machine_diagrams.sh"; exit 1; fi
