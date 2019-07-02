5de8fc19a037ec77864a415762762c62af953fd6
Martina Rivizzigno <martina@rivizzigno.it>
2019-07-02 14:57:16 +0200

    update generated launch file for structure core and safe landing planner

--- safe_landing_planner/tools/generate_launchfile.sh
+++ safe_landing_planner/tools/generate_launchfile.sh
@@ -57,13 +57,20 @@ for camera in $CAMERA_CONFIGS; do
 			    <node pkg="tf" type="static_transform_publisher" name="tf_$1"
 			       args="$4 $5 $6 $7 $8 $9 fcu $1_link 10"/>
 			    <include file="\$(find local_planner)/launch/rs_depthcloud.launch">
+             <arg name="required"              value="true"/>
 			       <arg name="namespace"             value="$1" />
 			       <arg name="tf_prefix"             value="$1" />
 			       <arg name="serial_no"             value="$3"/>
 			       <arg name="depth_fps"             value="$DEPTH_CAMERA_FRAME_RATE"/>
-			       <arg name="enable_pointcloud"     value="false"/>
-			       <arg name="enable_fisheye"        value="false"/>
 			    </include>
+
+          <!-- launch node to throttle depth images for logging -->
+          <node name="drop_$1_depth" pkg="topic_tools" type="drop" output="screen"
+            args="/$1/depth/image_rect_raw 29 30">
+          </node>
+          <node name="drop_$1_ir" pkg="topic_tools" type="drop" output="screen"
+            args="/$1/infra1/image_rect_raw 29 30">
+          </node>
 
 		EOM
 
@@ -71,24 +78,40 @@ for camera in $CAMERA_CONFIGS; do
 		echo "rosrun dynamic_reconfigure dynparam set /$1/stereo_module enable_auto_exposure 0
 		rosrun dynamic_reconfigure dynparam set /$1/stereo_module enable_auto_exposure 1
     rosrun dynamic_reconfigure dynparam set /$1/stereo_module visual_preset 4
-		" >> resource/realsense_params.sh
+		" >> resource/rqt_param_toggle.sh
 
 	elif  [[ $2 == "struct_core" ]]; then
 
 	cat >>    launch/safe_landing_planner_launch.launch <<- EOM
-			    <node pkg="tf" type="static_transform_publisher" name="tf_$1"
-			       args="$4 $5 $6 $7 $8 $9 fcu $1_FLU 10"/>
+      <node pkg="tf" type="static_transform_publisher" name="tf_$1" required="true"
+         args="$4 $5 $6 $7 $8 $9 fcu $1_map 10"/>
 
-			    <rosparam command="load" file="\$(find struct_core_ros)/launch/sc.yaml"/>
-			    <node pkg="struct_core_ros" type="sc" name="$1"/>
+      <include file="\$(find structure_core_ros_driver)/launch/sc.launch">
+         <arg name="name"                  value="$1"/>
+         <arg name="required"              value="true"/>
+         <arg name="serial_number"         value="$3"/>
+         <arg name="infrared_enable"       value="false" />
+         <arg name="depth_enable"          value="true" />
+         <arg name="frame_sync_enabled"    value="false" />
+         <arg name="depth_framerate"       value="$DEPTH_CAMERA_FRAME_RATE" />
+         <arg name="depth_apply_correction_before_stream"              value="true"/>
+      </include>
 
-          <!-- launch node to throttle depth images for logging -->
-          <node name="drop_sc_depth" pkg="topic_tools" type="drop" output="screen"
-             args="/sc/depth/image_rect 29 30 /sc/depth/image_rect_drop">
-          </node>
+      <node name="drop_$1_depth" pkg="topic_tools" type="drop" output="screen"
+         args="/$1/depth/image 29 30">
+         <remap from="/$1/depth/image_drop" to="/$1/depth/image_rect_raw_drop"/>
+      </node>
 
+      <node name="drop_$1_rgb" pkg="topic_tools" type="drop" output="screen"
+         args="/$1/rgb/image 29 30">
+      </node>
 
 		EOM
+
+    # Append to the struct core depth mode toggling
+		echo "rosrun dynamic_reconfigure dynparam set /$1_node depth_range_mode 3
+		" >> resource/rqt_param_toggle.sh
+
 	else
 	echo "Unknown camera type $2 in CAMERA_CONFIGS"
 	fi
@@ -122,7 +145,7 @@ cat >> launch/safe_landing_planner_launch.launch <<- EOM
     </node>
 
     <!-- switch off and on auto exposure of Realsense cameras, as it does not work on startup -->
-    <node name="set_RS_param" pkg="safe_landing_planner" type="realsense_params.sh" />
+    <node name="rqt_param_toggle" pkg="safe_landing_planner" type="rqt_param_toggle.sh" />
 
 </launch>
 
