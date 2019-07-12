6380f60545b25e3a4b1b4739bad068bb2bf1055f
Nico van Duijn <nico@auterion.com>
2019-07-12 16:20:36 +0200

    Move full-tree-print to simulation only

--- local_planner/src/nodes/star_planner.cpp
+++ local_planner/src/nodes/star_planner.cpp
@@ -133,9 +133,12 @@ void StarPlanner::buildLookAheadTree() {
   ROS_INFO("\033[0;35m[SP]Tree (%lu nodes, %lu path nodes, %lu expanded) calculated in %2.2fms.\033[0m", tree_.size(),
            path_node_positions_.size(), closed_set_.size(),
            static_cast<double>((std::clock() - start_time) / static_cast<double>(CLOCKS_PER_SEC / 1000)));
+
+  #ifndef DISABLE_SIMULATION // For large trees, this could be very slow!
   for (int j = 0; j < path_node_positions_.size(); j++) {
     ROS_DEBUG("\033[0;35m[SP] node %i : [ %f, %f, %f]\033[0m", j, path_node_positions_[j].x(),
               path_node_positions_[j].y(), path_node_positions_[j].z());
   }
+  #endif
 }
 }
