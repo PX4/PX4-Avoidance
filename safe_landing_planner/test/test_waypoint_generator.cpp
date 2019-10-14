d71b6135f35d0b1368f1e6dcfe1be8ec2e9fc7a6
Martina Rivizzigno <martina@rivizzigno.it>
2019-10-14 11:56:43 +0200

    improve safe_landing_planner waypoint_generator code coverage (#503)

--- safe_landing_planner/test/test_waypoint_generator.cpp
+++ safe_landing_planner/test/test_waypoint_generator.cpp
@@ -123,6 +123,12 @@ TEST_F(WaypointGeneratorTests, loiter_to_evaluateGrid) {
   // THEN: the state should go to Loiter
   ASSERT_EQ(SLPState::LOITER, getState());
 
+  // WHEN: data isn't ready
+  grid_slp_seq_ = 18;
+  calculateWaypoint();
+  // THEN: the state should go to EvaluateGrid
+  ASSERT_EQ(SLPState::LOITER, getState());
+
   // WHEN: data is ready
   grid_slp_seq_ = 25;
   calculateWaypoint();
@@ -159,6 +165,43 @@ TEST_F(WaypointGeneratorTests, evaluateGrid_to_goToLand) {
 
   // WHEN: a landing area is available in the current grid
   can_land_hysteresis_result_.fill(1);
+  calculateWaypoint();
+  // THEN: the state should go to goto_land
+  ASSERT_EQ(SLPState::GOTO_LAND, getState());
+  // AND: published position should be loiter_position is x, y, z and velocity (nan, nan, nan)
+  ASSERT_TRUE(published_velocity.array().isNaN().all());
+  ASSERT_EQ(Eigen::Vector2f(published_position.x() - position_.x(), published_position.y() - position_.y()).norm(), 0);
+}
+
+TEST_F(WaypointGeneratorTests, evaluateGrid_to_goToLand_with_full_grid_eval) {
+  // GIVEN: a basic waypoint generator, that switched to altitude change after
+  // first iteration
+  ASSERT_EQ(SLPState::GOTO, getState());
+
+  // WHEN: we are above our landing location and at the correct altitude
+  goal_ << 10, 10, 0;
+  position_ << 10, 10, 4.5;
+  is_land_waypoint_ = true;
+
+  // THEN: the state should go to altitudeChange
+  calculateWaypoint();
+  ASSERT_EQ(SLPState::ALTITUDE_CHANGE, getState());
+
+  // THEN: the state should go to Loiter
+  calculateWaypoint();
+  ASSERT_EQ(SLPState::LOITER, getState());
+
+  // WHEN: data is ready
+  grid_slp_seq_ = 25;
+  calculateWaypoint();
+  // THEN: the state should go to EvaluateGrid
+  ASSERT_EQ(SLPState::EVALUATE_GRID, getState());
+
+  // WHEN: a landing area is available in the current grid but not at the current position
+  can_land_hysteresis_result_.fill(0);
+  can_land_hysteresis_result_.topLeftCorner(can_land_hysteresis_result_.rows() / 2,
+                                            can_land_hysteresis_result_.cols() / 2) =
+      Eigen::MatrixXi::Ones(can_land_hysteresis_result_.rows() / 2, can_land_hysteresis_result_.cols() / 2);
   calculateWaypoint();
   // THEN: the state should go to goto_land
   ASSERT_EQ(SLPState::GOTO_LAND, getState());
@@ -259,4 +302,35 @@ TEST_F(WaypointGeneratorTests, land_transitions) {
   ASSERT_TRUE(std::isnan(published_velocity.x()));
   ASSERT_TRUE(std::isnan(published_velocity.y()));
   ASSERT_FALSE(std::isnan(published_velocity.z()));
+}
+
+TEST_F(WaypointGeneratorTests, goTo_repeat_spiral) {
+  // GIVEN: a basic waypoint generator in GOTO state
+  ASSERT_EQ(SLPState::GOTO, getState());
+
+  // WHEN: we are above our landing location but we decide we cannot land
+  is_land_waypoint_ = true;
+  decision_taken_ = true;
+  can_land_ = false;
+  goal_ << 10, 10, 0;
+  position_ << 10, 10, 4.5;
+  loiter_position_ << 10, 10, 4.5;
+  spiral_width_ = 2;
+  std::vector<Eigen::Vector2f> spiral_wp = {Eigen::Vector2f(34.f, 10.f),  Eigen::Vector2f(34.f, 34.f),
+                                            Eigen::Vector2f(10.f, 34.f),  Eigen::Vector2f(-14.f, 34.f),
+                                            Eigen::Vector2f(-14.f, 10.f), Eigen::Vector2f(-14.f, -14.f),
+                                            Eigen::Vector2f(10.f, -14.f), Eigen::Vector2f(34.f, -14.f),
+                                            Eigen::Vector2f(58.f, 10.f),  Eigen::Vector2f(58.f, 58.f)};
+
+  // THEN: the state should remain GOTO and the goal should change to perform the spiral
+  for (int i = 0; i < 10; i++) {
+    decision_taken_ = true;
+    can_land_ = false;
+    calculateWaypoint();
+    explorarion_is_active_ = false;
+    ASSERT_EQ(SLPState::GOTO, getState());
+    position_ = goal_;
+    EXPECT_FLOAT_EQ(spiral_wp[i].x(), goal_.x());
+    EXPECT_FLOAT_EQ(spiral_wp[i].y(), goal_.y());
+  }
 }
