8110e0d0527a9902a7198757a223c94e93dc2ddd
TSC21 <n.marques21@hotmail.com>
2020-04-22 11:48:36 +0100

    local_planner: port local_planner_node_main

--- local_planner/src/nodes/local_planner_node_main.cpp
+++ local_planner/src/nodes/local_planner_node_main.cpp
@@ -1,15 +1,11 @@
-#include <nodelet/loader.h>
-#include <ros/ros.h>
+#include <chrono>
+#include <local_planner/local_planner.h>
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "local_planner_nodelet");
+int main(int argc, char* argv[]) {
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<LocalPlanner>());
 
-  nodelet::Loader nodelet;
-  nodelet::M_string remap(ros::names::getRemappings());
-  nodelet::V_string nargv;
-  std::string nodelet_name = ros::this_node::getName();
-  nodelet.load(nodelet_name, "LocalPlannerNodelet", remap, nargv);
-  ros::spin();
-
+  rclcpp::shutdown();
   return 0;
 }
