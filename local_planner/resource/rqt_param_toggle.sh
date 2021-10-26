15bb901c1c9aac00a318f36dabe551c517991808
Ata Parlar <ataparlars5@gmail.com>
2021-10-26 20:00:46 +0300

    just a refresh

--- local_planner/resource/rqt_param_toggle.sh
+++ local_planner/resource/rqt_param_toggle.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Toggle or set rqt parameters
-rosrun dynamic_reconfigure dynparam set /sc_node depth_range_mode 3
+
+		rosrun dynamic_reconfigure dynparam set /camera/stereo_module enable_auto_exposure 0
+		rosrun dynamic_reconfigure dynparam set /camera/stereo_module enable_auto_exposure 1
 		
